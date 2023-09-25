d2f6b8d0f1beaac7434401344da64db2ef449d1d
kangs <seunggu.kang@carsync.de>
2023-09-25 13:57:01 +0900

    struct_padding example added, debug error fixed

--- memory_access_methods.c
+++ memory_access_methods.c
@@ -35,39 +35,49 @@ typedef struct
 } memory_map_aligned;
 
 
+typedef struct
+{
+    volatile uint32_t CTRL;
+    volatile uint32_t LOAD;
+    volatile uint32_t VAL;
+    volatile const uint32_t CALIB;
+}My_SysTick_Type;
+
 
 int main(){
     PLL_Init_84MHZ();
-    /*Uncomment whichever section you wish to use 
-	  Uncomment any one Method at a time
-	*/
+
+    /*
     //////////// Method 1 /////////////////////////////////
     #define SCS_BASE        0xE000E000UL   
-    #define STK_CTRL        (SCS_BASE +  0x0010UL)  
-    #define STK_LOAD        (SCS_BASE +  0x0014UL)
-    #define STK_VAL         (SCS_BASE +  0x0018UL)
-    #define STK_CALIB       (SCS_BASE +  0x001CUL)
-
+    #define STK_CTRL        (SCS_BASE + 0x0010UL)  
+    #define STK_LOAD        (SCS_BASE + 0x0014UL)
+    #define STK_VAL         (SCS_BASE + 0x0018UL)
+    #define STK_CALIB       (SCS_BASE + 0x001CUL)
     #define ADDRW(x)        (*((volatile unsigned int *)(x))) 
     #define ADDRH(x)        (*((volatile unsigned short *)(x)))
     #define ADDRB(x)        (*((volatile unsigned char *)(x)))
     #define CTRL(x)         0x0000
-
+    
     uint32_t value1 = ADDRH(STK_CTRL);
+    uint32_t value2 = ADDRW(STK_LOAD);
     ADDRH(STK_CTRL) = 5;
+    ADDRW(STK_LOAD) = 0x000000FF;
     value1 = ADDRH(STK_CTRL);
-	
-    /*
+    value2 = ADDRW(STK_LOAD);
+	*/
+   
     ///////////// Method 2 ////////////////////////////////
     //memory_map_aligned * EXTRACT_DATA;
     //EXTRACT_DATA = (memory_map_aligned *) &data_buffer_in;
-    #define EXTRACT_DATA       ((memory_map_unaligned_packed *) &data_buffer_in)
-    uint32_t array_size = sizeof(memory_map_unaligned_packed);
-    uint32_t first = EXTRACT_DATA->first_element;
-    uint32_t second = EXTRACT_DATA->second_element;
-    uint32_t third = EXTRACT_DATA->third_element;
-    uint32_t fourth = EXTRACT_DATA->fourth_element;
-    */
+    #define SysTick_BASE 0xE000E010UL
+    #define SysTick ((My_SysTick_Type *) SysTick_BASE)
+
+    uint32_t value1 = SysTick->CTRL;
+    SysTick->CTRL = 5;
+    uint32_t value2 = SysTick->LOAD;
+    SysTick->LOAD = 0x000000FF;
+
     return 0;
 }
 
